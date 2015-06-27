115550414961f19dedadf17aa193cfbf23183b18
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-06-26 21:49:37 -0600

    adds user new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  
+  root 'static_pages#home'
+  get '/login', to: 'static_pages#login'
+  resources 'lists'
+  resources 'users'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626225324) do
+ActiveRecord::Schema.define(version: 20150627021543) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +29,13 @@ ActiveRecord::Schema.define(version: 20150626225324) do
     t.date     "due_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
